beb0b056ef5572f165f983e4c8554e9aaef9a3b1
Hayden <hayden@example.com>
2023-02-09 18:35:57 +0000

    change various values in config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Medium:pixelsize=19:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Medium:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 6;
 
 /*
